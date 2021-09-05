719145d8eef7cc684bb7500f6d8d632e76c63808
kohishibashi <trendy8455@gmail.com>
2021-09-06 02:46:19 +0900

    part4

--- webtestapp/views.py
+++ webtestapp/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from .forms import TestForm
+
 
 def index(request):
     my_dict = {
         'insert_something':"views.pyのinsert_something部分です。",
         'name':'Bashi',
         'test_titles': ['title 1', 'title 2', 'title 3'],
+        'form': TestForm(),
+        'insert_forms':'初期値',
     }
+    if (request.method == 'POST'):
+        my_dict['insert_forms'] = '文字列:' + request.POST['text'] + '\n整数型:' + request.POST['num']
+        my_dict['form'] = TestForm(request.POST)
+
     return render(request, 'webtestapp/index.html', my_dict)
