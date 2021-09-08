f03490bf3194405223e28c5eca59eb233b6e4315
kohishibashi <trendy8455@gmail.com>
2021-09-09 01:42:35 +0900

    part5

--- webtestapp/urls.py
+++ webtestapp/urls.py
@@ -5,5 +5,8 @@ app_name='webtestapp'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('info', views.info, name='info'),    
+    path('info', views.info, name='info'),
+    path('create', views.create, name='create'),
+    path('update/<int:num>', views.update, name='update'),
+    path('delete/<int:num>', views.delete, name='delete'),
 ]
